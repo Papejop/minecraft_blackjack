4057179fc11125191c51a4fd261b18cb537bce39
Papejop <PiotrLy2001@gmail.com>
2024-12-21 23:31:20 +0100

    fix: implement tag generator package

--- src/main/java/net/me/minecraft_blackjack/datagen/ModItemTagGenerator.java
+++ src/main/java/net/me/minecraft_blackjack/datagen/ModItemTagGenerator.java
@@ -1,5 +1,24 @@
 package net.me.minecraft_blackjack.datagen;
 
-public class ModItemTagGenerator {
-    
+import java.util.concurrent.CompletableFuture;
+
+import org.jetbrains.annotations.Nullable;
+
+import net.me.minecraft_blackjack.blackjack;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+public class ModItemTagGenerator extends ItemTagsProvider {
+    public ModItemTagGenerator(PackOutput p_275343_, CompletableFuture<HolderLookup.Provider> p_275729_,
+                               CompletableFuture<TagLookup<Block>> p_275322_, @Nullable ExistingFileHelper existingFileHelper) {
+        super(p_275343_, p_275729_, p_275322_, blackjack.MODID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider pProvider) {
+
+    }
 }
